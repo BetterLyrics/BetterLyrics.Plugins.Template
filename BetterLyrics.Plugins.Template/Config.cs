319769ed232e6531cdd3bfc070904b6cd7a6950a
jayfunc <founchoo@outlook.com>
2026-07-01 15:10:23 +0800

    init code

---
+++ BetterLyrics.Plugins.Template/Config.cs
@@ -0,0 +1,15 @@
+﻿using BetterLyrics.Sdk.Abstractions.Plugins;
+using System.ComponentModel.DataAnnotations;
+
+namespace BetterLyrics.Plugins.Template
+{
+    public class Config : PluginConfigBase
+    {
+        [Display(Name = "Example Setting Name", Description = "Example Setting Description")]
+        public string ExampleSetting
+        {
+            get => Get("Example Value");
+            set => Set(value);
+        }
+    }
+}
---
+++ BetterLyrics.Plugins.Template/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BetterLyrics.Sdk.Abstractions.Plugins;
+using BetterLyrics.Sdk.Interfaces.Plugins;
+using BetterLyrics.Sdk.Models.Lyrics;
+
+namespace BetterLyrics.Plugins.Template
+{
+    public class Plugin : PluginBase<Config>, ILyricsSource
+    {
+        public override string Title { get; set; } = "TODO: Plugin Name";
+
+        public Task<LyricsSearchResult> GetLyricsAsync(string title, string artist, string album, double duration, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override async Task OnInitializeAsync()
+        {
+            // Do any initialization work here, such as setting up services, loading resources, etc.
+
+            // Context.PluginDirectory; // This is the directory where your plugin is located. You can use it to load resources or configuration files.
+            // Config.ExampleSetting = "Example Value"; // This is how you can access and modify your plugin's configuration settings.
+        }
+
+        protected override async Task OnShutdownAsync()
+        {
+            // Do any cleanup work here, such as disposing of resources, saving state, etc.
+        }
+    }
+}
